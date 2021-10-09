3a2ed40376c51640dab640c2c9ec8482e056aeb4
Radosław Korzeniewski <radoslaw@korzeniewski.net>
2021-10-09 17:51:13 +0200

    metaplugin: Add missing files.

---
+++ bacula/src/plugins/fd/pluginlib/metaplugin_metadata.cpp
@@ -0,0 +1,47 @@
+/*
+   Bacula(R) - The Network Backup Solution
+
+   Copyright (C) 2000-2023 Kern Sibbald
+
+   The original author of Bacula is Kern Sibbald, with contributions
+   from many others, a complete list can be found in the file AUTHORS.
+
+   You may use this file and others of this release according to the
+   license defined in the LICENSE file, which includes the Affero General
+   Public License, v3.0 ("AGPLv3") and some additional permissions and
+   terms pursuant to its AGPLv3 Section 7.
+
+   This notice must be preserved when any source code is
+   conveyed and/or propagated.
+
+   Bacula(R) is a registered trademark of Kern Sibbald.
+ */
+/**
+ * @file metaplugin_accurate.cpp
+ * @author Radosław Korzeniewski (radoslaw@korzeniewski.net)
+ * @brief This is a metadata (ACL, XATTR, METADATA) handling for metaplugin.
+ * @version 1.0.0
+ * @date 2021-09-20
+ *
+ * @copyright Copyright (c) 2021 All rights reserved. IP transferred to Bacula Systems according to agreement.
+ */
+
+#include "metaplugin_metadata.h"
+
+
+/*
+ * libbac uses its own sscanf implementation which is not compatible with
+ * libc implementation, unfortunately.
+ * use bsscanf for Bacula sscanf flavor
+ */
+#ifdef sscanf
+#undef sscanf
+#endif
+
+namespace metaplugin
+{
+namespace metadata
+{
+
+}  // namespace metadata
+}  // namespace metaplugin
